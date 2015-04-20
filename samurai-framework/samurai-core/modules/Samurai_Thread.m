e4d117e78f84e60b782da6f70390bcfda814ec17
uxyheaven <uxyheaven@163.com>
2015-04-20 13:57:09 +0800

    update SamuraiGCD

--- samurai-framework/samurai-core/modules/Samurai_Thread.m
+++ samurai-framework/samurai-core/modules/Samurai_Thread.m
@@ -36,7 +36,35 @@
 // ----------------------------------
 // Source code
 // ----------------------------------
+#pragma mark -
 
+@interface SamuraiGCD()
+@end
+
+@implementation SamuraiGCD
+
+@def_singleton( SamuraiGCD )
+
+@def_prop_strong(dispatch_queue_t,			foreQueue );
+@def_prop_strong(dispatch_queue_t,			backSerialQueue );
+@def_prop_strong(dispatch_queue_t,			backConcurrentQueue );
+@def_prop_strong(dispatch_queue_t,			writeFileQueue );
+
+- (id)init
+{
+    self = [super init];
+    if ( self )
+    {
+        _foreQueue           = dispatch_get_main_queue();
+        _backSerialQueue     = dispatch_queue_create( "com.samurai.backSerialQueue", DISPATCH_QUEUE_SERIAL );
+        _backConcurrentQueue = dispatch_queue_create( "com.samurai.backConcurrentQueue", DISPATCH_QUEUE_CONCURRENT );
+        _writeFileQueue      = dispatch_queue_create( "com.samurai.writeFileQueue", DISPATCH_QUEUE_SERIAL );
+    }
+    
+    return self;
+}
+
+@end
 // ----------------------------------
 // Unit test
 // ----------------------------------
